3a35149845b82107968322cba5c692861bfbe5cf
Worameth Siriwarin <worameth.siri@kmutt.ac.th>
2025-04-16 22:39:22 +0700

    second commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -70,7 +70,33 @@ router.post('/login', async (req, res) => {
   
 
 
+  // รายชื่อผู้โดยสารของผู้ใช้ ใช้ ID ในการเช็ค
+router.get('/:userID/passengers', async (req, res) => {
+  const { userID } = req.params;
+  const db = req.db;
+  const [passengers] = await db.execute(`
+    SELECT p.* FROM Passengers p
+    JOIN UserPassengers up ON p.passengerID = up.passengerID
+    WHERE up.userID = ?
+  `, [userID]);
 
+  res.json(passengers);
+});
+
+// ประวัติการจอง
+router.get('/:userID/bookings', async (req, res) => {
+  const { userID } = req.params;
+  const db = req.db;
+  const [bookings] = await db.execute(`
+    SELECT b.*, f.label AS flightLabel, f.departTime, f.arrivalTime
+    FROM Bookings b
+    JOIN Flights f ON b.flightID = f.flightID
+    WHERE b.userID = ?
+    ORDER BY b.bookingDate DESC
+  `, [userID]);
+
+  res.json(bookings);
+});
 
 
 
